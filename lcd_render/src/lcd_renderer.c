61f4eee7439282b5c4305f97b30d72ad8d2d9975
itsZero (Chien-An Cho) <itszero@gmail.com>
2012-12-12 00:03:17 -0500

    lcd_renderer: now render image by the JSON

--- lcd_render/src/lcd_renderer.c
+++ lcd_render/src/lcd_renderer.c
@@ -5,8 +5,18 @@
 #include FT_FREETYPE_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+#include "parson.h"
 
 FT_Library ft_lib;
+
+void lcd_draw_text(cairo_t *ctx, float x, float y, const char *str)
+{
+  cairo_move_to(ctx, x, y);
+  cairo_set_source_rgb(ctx, 0, 0, 0);
+  cairo_show_text(ctx, str);  
+}
 
 void lcd_draw_progress_bar(cairo_t *ctx, float x, float y, float width, float height, float progress)
 {
@@ -29,29 +39,118 @@ void lcd_draw_progress_bar(cairo_t *ctx, float x, float y, float width, float he
   cairo_fill(ctx);
 }
 
-int main(int argc, char *argv[])
+cairo_font_face_t *load_font_for_cairo(char *fname, int index)
+{
+  int error;
+  FT_Face ftToLoad;
+
+  error = FT_New_Face(ft_lib, fname, index, &ftToLoad);
+  if (error)
+  {
+    fprintf(stderr, "lcd_renderer: Failed to load font\n");
+    return NULL;
+  }
+
+  return cairo_ft_font_face_create_for_ft_face(ftToLoad, 0);
+}
+
+void init_libs()
 {
   int error;
 
   error = FT_Init_FreeType(&ft_lib);
   if (error)
   {
-    fprintf(stderr, "Failed to init freetype2\n");
+    fprintf(stderr, "lcd_renderer: Failed to init freetype2\n");
     exit(-1);
   }
+}
 
-  FT_Face ftMetaWatch;
-  cairo_font_face_t *ffMetaWatch;
+cairo_status_t write_png_stream_to_stdout(void *closure, const unsigned char *data, unsigned int length)
+{
+  if (fwrite(data, 1, length, stdout) == length)
+    return CAIRO_STATUS_SUCCESS;
+  else
+    return CAIRO_STATUS_WRITE_ERROR;
+}
 
-  error = FT_New_Face(ft_lib, "./metawatch_8pt.ttf", 0, &ftMetaWatch);
-  if (error)
+void draw_image(cairo_t *ctx, JSON_Object *root)
+{
+  JSON_Array *layout = json_object_get_array(root, "layout");
+  JSON_Object *values = json_object_get_object(root, "values");
+
+  size_t layout_length = json_array_get_count(layout);
+  fprintf(stderr, "lcd_renderer: layout has %zd objects\n", layout_length);
+  for(size_t i=0;i<layout_length;i++)
   {
-    fprintf(stderr, "Failed to load font\n");
-    exit(-1);
+    JSON_Object *obj = json_array_get_object(layout, i);
+    const char *type = json_object_get_string(obj, "type");
+    fprintf(stderr, "lcd_renderer: [layout] obj #%zd - %s\n", i, type);
+    if (strcmp(type, "text") == 0)
+    {
+      JSON_Array *origin = json_object_get_array(obj, "origin");
+      float x = (float)json_array_get_number(origin, 0), y = (float)json_array_get_number(origin, 1);
+      const char *value_key = json_object_get_string(obj, "value");
+      const char *str = json_object_get_string(values, value_key);
+
+      fprintf(stderr, "lcd_renderer: [layout] origin: (%.1f, %.1f) str: %s (key: %s)\n", x, y, str, value_key);
+      lcd_draw_text(ctx, x, y, str);      
+    }
+    else if (strcmp(type, "progressbar") == 0)
+    {
+      JSON_Array *origin = json_object_get_array(obj, "origin");
+      float x = (float)json_array_get_number(origin, 0), y = (float)json_array_get_number(origin, 1);
+      JSON_Array *size = json_object_get_array(obj, "size");
+      float w = (float)json_array_get_number(size, 0), h = (float)json_array_get_number(size, 1);
+      const char *value_key = json_object_get_string(obj, "value");
+      float prg = (float)json_object_get_number(values, value_key);
+
+      fprintf(stderr, "lcd_renderer: [layout] origin: (%.1f, %.1f) size: (%.1f, %.1f) progress: %.1f (key: %s)\n", x, y, w, h, prg, value_key);
+      lcd_draw_progress_bar(ctx, x, y, w, h, prg);
+    }
+  }
+}
+
+char* read_all_from_stdin()
+{
+  char buf[1024];
+  char *content = malloc(sizeof(char) * 1024);
+  content[0] = '\0';
+
+  size_t sz = 1;
+  while(fgets(buf, 1024, stdin))
+  {
+    char *old = content;
+    sz += strlen(buf);
+    content = realloc(content, sz);
+    if (content == NULL)
+    {
+      fprintf(stderr, "lcd_renderer: Failed to reallocate more space for content\n");
+      free(old);
+      exit(-1);
+    }
+    strcat(content, buf);
   }
 
-  ffMetaWatch = cairo_ft_font_face_create_for_ft_face(ftMetaWatch, 0);
+  return content;
+}
 
+int main(int argc, char *argv[])
+{
+  init_libs();
+
+  char* strJSON = read_all_from_stdin();
+  fprintf(stderr, "lcd_renderer: read %zd bytes JSON\n", strlen(strJSON));
+  JSON_Value *valRoot = json_parse_string(strJSON);
+  free(strJSON);
+  if (valRoot == NULL)
+  {
+    fprintf(stderr, "lcd_renderer: Failed to parse JSON input\n");
+    exit(-1);
+  }
+  JSON_Object *root = json_value_get_object(valRoot);
+
+  cairo_font_face_t *ffMetaWatch = load_font_for_cairo("./metawatch_8pt.ttf", 0);
   cairo_surface_t *cs;
   cs = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 256, 64);
 
@@ -68,38 +167,16 @@ int main(int argc, char *argv[])
   cairo_set_font_options(ctx, cfo);
   cairo_set_font_face(ctx, ffMetaWatch);
   cairo_set_font_size(ctx, 8);
-  
-  cairo_move_to(ctx, 0.0, 8.0);
-  cairo_set_source_rgb(ctx, 0, 0, 0);
-  cairo_show_text(ctx, "CPU: 50%");
 
-  lcd_draw_progress_bar(ctx, 60.0, 0.0, 127.0 - 60.0, 8.0, 0.5);
-
-  cairo_move_to(ctx, 128.0, 8.0);
-  cairo_set_source_rgb(ctx, 0, 0, 0);
-  cairo_show_text(ctx, "MEM: 90% 11G");
-
-  lcd_draw_progress_bar(ctx, 200.0, 0.0, 256.0 - 200.0, 8.0, 0.9);
-
-  cairo_move_to(ctx, 0.0, 16.0);
-  cairo_set_source_rgb(ctx, 0, 0, 0);
-  cairo_show_text(ctx, "disk1: 92% 33G");
-
-  lcd_draw_progress_bar(ctx, 70.0, 9.0, 127.0 - 70.0, 8.0, 0.92);
-
-  cairo_move_to(ctx, 0.0, 56.0);
-  cairo_set_source_rgb(ctx, 0, 0, 0);
-  cairo_show_text(ctx, "up 2 days, 23:48, 5 users, load: 1.33 1.34 1.40");
-
-  cairo_move_to(ctx, 0.0, 64.0);
-  cairo_set_source_rgb(ctx, 0, 0, 0);
-  cairo_show_text(ctx, "2012/12/10 11:14PM // retina // Mac OS X 10.8 Mt. Lion");
+  draw_image(ctx, root);
 
   cairo_font_options_destroy(cfo);
   cairo_destroy(ctx);
 
-  cairo_surface_write_to_png(cs, "1.png");
+  cairo_surface_write_to_png_stream(cs, write_png_stream_to_stdout, NULL);
   cairo_surface_flush(cs);
   cairo_surface_destroy(cs);
   cairo_font_face_destroy(ffMetaWatch);
+
+  json_value_free(valRoot);
 }
